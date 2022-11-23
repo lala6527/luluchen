136a4159ab62d70232b314832c47ee68f106fd1d
KenHuangTX <ken_huang@txone.com>
2022-11-23 15:47:07 +0800

    refactor: project structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src="/"></img>
+      <nav>
+        <ul>
+          <li>
+            <Link href="home">Home</Link>
+          </li>
+          <li>
+            <Link href="about">About</Link>
+          </li>
+          <li>
+            <Link href="/"></Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
