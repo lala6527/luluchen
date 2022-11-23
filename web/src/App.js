477c6e8b348ad6bdb213de03bbce266d65154a26
chenjihan <jihan.chen@ctitv.com.tw>
2022-11-23 15:27:50 +0800

    featHeaderandFooter

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import{BrowserRouter ,Route ,Routes} from "react-router-dom";
+import { Header,Footer} from "./components";
 
-function App() {
+import './App.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+ <Header />
+ <Footer />
+ </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
