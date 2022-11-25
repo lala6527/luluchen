0a415bc04e72c40a71e09e4b5a346791ebd00b10
chenjihan <jihan.chen@ctitv.com.tw>
2022-11-25 19:01:26 +0800

    feat:image

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,15 @@ import styles from "./footer.module.scss";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>LULUCHEN</p>
+      <div className={styles.container}>
+        <div className={styles.footeritem}>
+       <a href="https://github.com/lala6527/luluchen.git" target="_blank"><img src="images/Githubicon.png"  alt="Github"/></a>
+       <a href="https://www.linkedin.com/in/jihan-chen-b64850257" target="_blank"><img src="images/Linkedinicon.png" alt="Linkedin"/></a>
+       <a href="https://www.instagram.com/bonnie_55417/" target="_blank"><img src="images/Igicon.png" alt="IG"/></a>
+       </div>
+      <p>© LuLu Chen 2022 Copyright. All Rights Reserved.</p>
+      </div>
     </footer>
+
   );
 };
--- src/pages/luluchen/index.js
+++ src/pages/luluchen/index.js
@@ -1,5 +1,11 @@
+import styles from "./luluchen.module.scss";
+
 export const LuluChen = () => {
-  return <div>
-    luluchen
-  </div>;
+  return (
+  <div className={styles.luluchen}>
+    <div className={styles.titlepage}>
+    <img src="./images/titlepage.png" alt="封面"></img>
+    </div>
+  </div>
+  )
 };
