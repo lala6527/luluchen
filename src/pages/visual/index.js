d634d2a38e9e2dd0cb1a24ad0ebdfde975048b01
chenjihan <jihan.chen@ctitv.com.tw>
2022-12-15 19:22:30 +0800

    featvisual3

--- src/pages/visual/index.js
+++ src/pages/visual/index.js
@@ -3,27 +3,66 @@ import styles from "./visual.module.scss";
 export const Visual = () =>{
     return(
         <div className={styles.visual}>
-         <div className="container">
+        <div className="container">
         <h3>Visual Portfolio</h3>
         <p>Succeed online with a website that is fast, easy to use, and built with best practices and a passion for design.</p>
         <div className={styles.grid}>
         <div className={styles.card}>
-        <div className={styles.desc}> 
-        <h3>Project Name</h3>
-        <p>Visal project description Visal project description Visal project description</p>
-        <a href="#" >View More</a>
+		    <img src="./images/visual_01.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
         </div>
-        <img src="./images/visual_01.png" alt="visual" />   
-
+        <div className={styles.card}>
+		    <img src="./images/visual_02.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
         </div>
-        <a href="#" className={styles.card}><img src="./images/visual_02.png" alt="visual" /></a> 
-        <a href="#" className={styles.card}><img src="./images/visual_03.png" alt="visual" /></a> 
-        <a href="#" className={styles.card}><img src="./images/visual_04.png" alt="visual" /></a> 
-        <a href="#" className={styles.card}><img src="./images/visual_05.png" alt="visual" /></a> 
-        <a href="#" className={styles.card}><img src="./images/visual_06.png" alt="visual" /></a> 
+        <div className={styles.card}>
+		    <img src="./images/visual_03.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
         </div>
+        <div className={styles.card}>
+		    <img src="./images/visual_04.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
         </div>
+        <div className={styles.card}>
+		    <img src="./images/visual_05.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
         </div>
+        <div className={styles.card}>
+		    <img src="./images/visual_06.png" />
+                <div className={styles.info}>
+                    <h1>Project Name</h1>
+                        <p>Visal project description Visal project description Visal project description</p> 
+			                <button>View More</button> 
+                </div>
+        </div>
+        
+     </div>
+ </div>
+  </div>   
+    
+      
+     
+       
 
        
     )
