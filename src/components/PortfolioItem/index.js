3fa1842522fd8ef964218f6666999d78183860a0
Ken Huang <bluetch@gmail.com>
2022-12-22 19:46:04 +0800

    fix: portfolio image css

--- src/components/PortfolioItem/index.js
+++ src/components/PortfolioItem/index.js
@@ -1,21 +1,20 @@
 import styles from './portfolioItem.module.scss';
 
-export const PortfolioItem =(props) =>{
-return( 
-  <section>
-    <div className={styles.flexItem}>
-    <h2>{props.title}</h2>
-      <p>{props.summary}</p>
-      <div className={styles.tag}> <span>JavaScript</span><span>HTML</span><span>CSS/SASS</span><span>React</span></div>
-      <a href="#">View detail</a>
-     </div>
-      <a href="#">
-     <img src={props.img} alt={props.title} />
-     </a>
-   
-</section>
+export const PortfolioItem = (props) => {
+  return (
+    <section>
+      <div className={styles.flexItem}>
+        <h2>{props.title}</h2>
+        <p>{props.summary}</p>
+        <div className={styles.tag}> <span>JavaScript</span><span>HTML</span><span>CSS/SASS</span><span>React</span></div>
+        <a href="#">View detail</a>
+      </div>
+      <a href="#" className={styles.img}>
+        <img src={props.img} alt={props.title} />
+      </a>
+    </section>
 
 
 
-)
+  )
 }
