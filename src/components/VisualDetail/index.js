f20efda9d64c8c5157712b6574cd2927f3ae619c
kenhuang <kenhuang@TX-kenhuang-M1.local>
2023-02-02 08:41:56 +0800

    fix: visual detail structure

---
+++ src/components/VisualDetail/index.js
@@ -0,0 +1,17 @@
+import styles from "./visualDetailItem.module.scss";
+
+export const VisualDetailItem = (props) => {
+  return (
+    <div className={styles.visualDetailItem}>
+      <img src={props.background} alt={props.title} />
+      <div className="container">
+        <h2 className={styles.visualTitle}>{props.title}</h2>
+        <p className={styles.visualSummary}>{props.summary}</p>
+        <h3 className={styles.visualSubtitle}>{props.subtitle}</h3>
+        <p className={styles.visualContent}>{props.content}</p>
+        <h3 className={styles.visualSubtitleOne}>{props.subtitleOne}</h3>
+        <p className={styles.visualContentOne}>{props.contentOne}</p>
+      </div>
+    </div>
+  );
+};
