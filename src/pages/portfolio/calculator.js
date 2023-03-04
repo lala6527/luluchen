3f2d202e3de1bd3eff2f9e3456cfd8f552c7e470
Ken Huang <bluetch@gmail.com>
2023-03-04 11:31:30 +0800

    feat: init calculator button

--- src/pages/portfolio/calculator.js
+++ src/pages/portfolio/calculator.js
@@ -1,8 +1,34 @@
+import { bottomNavigationActionClasses } from '@mui/material';
+import styles  from './calculator.module.scss';
 
-export const Calculator = (props) =>{
-    return(
-        <div className="calculator">
-            <h1>Calculator</h1>
+export const Calculator = (props) => {
+  const btnValues= [
+    ["AC", "+/-", "%", "/"],
+    ["7", "8", "9", "x"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="]
+  ];
+  console.log(btnValues.flat());
+  return (
+    <div className="calculator">
+      <h1>Calculator</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.screen}>123</div>
+        <div className={styles.buttonBox}>
+          {btnValues.flat().map((btn,i)=>{
+            return(
+              <Button className={styles.btn}>{btn}</Button>
+            )
+          })}
         </div>
-    )
-    }
+      </div>
+    </div>
+  )
+}
+
+export const Button = ({className, children})=>{
+  return(
+    <button className={className}>{children}</button>
+  )
+}
